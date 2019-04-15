b6056ffc11f863f76776c7328af9a69ce54f02f1
natalia montes <natm94@uw.edu>
2019-04-15 12:20:34 -0700

    completed exercise

--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -4,9 +4,12 @@
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
 
+employees <- paste("employee", 1:100)
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
+
+salaries_2017 <- runif(100, 40000, 50000)
 
 
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
@@ -14,18 +17,25 @@
 # Again use the `runif()` function to pick 100 random numbers in that range.
 
 
+salary_adjustments <- runif(100, -5000, 10000)
+
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
+
+employee <- employees
+salaries <- data.frame(employee, salaries_2017, salary_adjustments)
 
 
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
 
+salaries$salary_2018 <- salaries_2017 + salary_adjustments
+
 
 # Add a column to the `salaries` data frame that has a value of `TRUE` if the 
 # person got a raise (their salary went up)
 
-
+salaries$got_raise <- salary_adjustments > 0
 
 ### Retrieve values from your data frame to answer the following questions
 ### Note that you should get the value as specific as possible (e.g., a single
@@ -33,30 +43,39 @@
 
 # What was the 2018 salary of Employee 57
 
+salaries[salaries$employee == "employee 57", "salary_2018"]
 
 # How many employees got a raise?
 
+sum(salaries$got_raise == TRUE)
 
 # What was the dollar value of the highest raise?
 
+highest_raise <- max(salaries$salary_adjustments)
 
 # What was the "name" of the employee who received the highest raise?
 
+salaries[salaries$salary_adjustments == highest_raise, "employee"]
 
 # What was the largest decrease in salaries between the two years?
 
+biggest_paycut <- min(salaries$salary_adjustments)
 
 # What was the name of the employee who recieved largest decrease in salary?
 
+salaries[salaries$salary_adjustments == biggest_paycut, "employee"]
 
 # What was the average salary change?
 
+mean(salary_adjustments)
 
 # For people who did not get a raise, how much money did they lose on average?
 
+avg_loss <- mean(salaries$salary_adjustments[salaries$got_raise == FALSE])
 
 ## Consider: do the above averages match what you expected them to be based on 
 ## how you generated the salaries?
 
 # Write a .csv file of your salary data to your working directory
 
+write.csv(salaries, "salaries.csv")
